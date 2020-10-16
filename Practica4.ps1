a6e2a98ce90f03aeb497d03c80bb6c44a3df4530
JulioDMed <67122090+JulioDMed@users.noreply.github.com>
2020-10-16 18:26:13 -0500

    Add files via upload

---
+++ Practica4.ps1
@@ -0,0 +1,16 @@
+﻿function Ayuda-Comandos{
+params{
+$lista = @(Get-Command),
+$keyword
+}
+for($i = 0; $i -lt $lista.count; $i++){
+   
+    if ($lista[$i] -match $keyword){
+        $lista[$i] | Get-Help
+  
+    }
+    
+}
+}
+
+Ayuda-Comandos -keywords "Write"
